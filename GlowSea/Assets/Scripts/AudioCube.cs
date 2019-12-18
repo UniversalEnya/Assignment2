5519e31b79a46f8c255e1878c2e1fcec8667360c
UniversalEnya <Emmaneesoneire@gmail.com>
2019-12-18 10:03:05 +0000

    Final

---
+++ GlowSea/Assets/Scripts/AudioCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCube : MonoBehaviour
+{
+    public int band;
+    public float startScale, scaleMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, (AudioAnalyzer.bands[band] * scaleMultiplier) + startScale);
+    }
+}
---
+++ GlowSea/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public int nextScene;
+
+    public void OnTriggerEnter()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
+
