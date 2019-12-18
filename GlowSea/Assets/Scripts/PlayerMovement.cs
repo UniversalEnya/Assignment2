68ba5042571086af7b4a7b695ee9997f70a20f13
UniversalEnya <Emmaneesoneire@gmail.com>
2019-12-18 04:58:07 +0000

    bla

--- GlowSea/Assets/Scripts/PlayerMovement.cs
+++ GlowSea/Assets/Scripts/PlayerMovement.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float speed = 5f;
+    float rotSpeed = 45f;
 
+    public void Update()
+    {
+        movementInput();
+    }
+
+    void movementInput()
+    {
+        
+    }
 }
